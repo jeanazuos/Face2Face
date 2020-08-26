c8611aa9ca10eb74fef3d940395c3eec299651ad
Jean Santos <jsantos@igspo.ogmaster.local>
2020-08-26 12:12:57 -0300

    first version of project

---
+++ app.py
@@ -0,0 +1,60 @@
+import face_recognition
+import numpy as np
+from PIL import Image, ImageDraw
+from IPython.display import display
+
+# This is an example of running face recognition on a single image
+# and drawing a box around each person that was identified.
+
+# Load a sample picture and learn how to recognize it.
+obama_image = face_recognition.load_image_file("obama.jpg")
+obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+# Load a second sample picture and learn how to recognize it.
+biden_image = face_recognition.load_image_file("biden.jpg")
+biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+
+# Create arrays of known face encodings and their names
+known_face_encodings = [
+    obama_face_encoding,
+    biden_face_encoding
+]
+known_face_names = [
+    "Barack Obama",
+    "Joe Biden"
+]
+# print('Learned encoding for', len(known_face_encodings), 'images.')
+
+
+# carregando imagem com um rosto desconhecido
+unknown_image = face_recognition.load_image_file("two_faces.jpg")
+
+face_locations = face_recognition.face_locations(unknown_image)
+face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+
+
+# convertemos para um formato PIL e entao podemos desenhar com Pillow Library
+pil_image = Image.fromarray(unknown_image)
+draw = ImageDraw.Draw(pil_image)
+
+for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+
+    name = "Unknow"
+
+    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+    best_match_index = np.argmin(face_distances)
+    if matches[best_match_index]:
+        name = known_face_names[best_match_index]
+
+    draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
+
+    text_width, text_height = draw.textsize(name)
+    draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
+    draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
+
+del draw
+
+display(pil_image)
+
+pil_image.save('result.jpg')
