72d17cba6d034d463307c1db0ed59c9959b42c13
Jean Santos <jsantos@igspo.ogmaster.local>
2020-08-26 12:36:37 -0300

    moved dir

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from PIL import Image, ImageDraw
 # and drawing a box around each person that was identified.
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
+obama_image = face_recognition.load_image_file("known_faces/obama.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
+biden_image = face_recognition.load_image_file("known_faces/biden.jpg")
 biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
 
 # Create arrays of known face encodings and their names
