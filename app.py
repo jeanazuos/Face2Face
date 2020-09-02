431dec388b6d8f2ff9be3dc15d288d9691aae7f6
Jean Santos <jsantos@igspo.ogmaster.local>
2020-09-01 21:24:10 -0300

    new version to run through terminal and new README

--- app.py
+++ app.py
@@ -1,57 +1,67 @@
+'''
+Construído baseado no exemplo da própria biblioteca face_recognition em:
+https://github.com/ageitgey/face_recognition/blob/master/examples/recognize_faces_in_pictures.py
+'''
+
 import face_recognition
-import numpy as np
-from PIL import Image, ImageDraw
+import os
 
-# This is an example of running face recognition on a single image
-# and drawing a box around each person that was identified.
+# To be continue or finish the while execute
+def try_new_test():
+    execute = input("\nGostaria de fazer um novo teste?\nDigite 1 para SIM ou 0 para NAO:\n")
+    if execute == '1':
+        execute = True
+        os.system('cls' if os.name == 'nt' else 'clear')
+    else:
+        execute = False
+        print("\n---FIM---")
+        quit()
+    return execute
 
-# Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("known_faces/obama.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+# Run menu
+execute = True
+while execute == True:
+    print("\n------ TESTE DE BIOMETRIA FACIAL ------\n")
 
-# Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("known_faces/biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+    # Set the config path and name files
+    main_path = "TESTE_DE_BIOMETRIA_FACIAL"
+    image_path = input("Insira o nome da pasta a ser analisado EX: 'Item_7':\n")
+    image_name_one = "Imagem_1.JPG"
+    image_name_two = "Imagem_2.JPG"
 
-# Create arrays of known face encodings and their names
-known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding
-]
-known_face_names = [
-    "Barack Obama",
-    "Joe Biden"
-]
-# print('Learned encoding for', len(known_face_encodings), 'images.')
+    # Load the jpg files into numpy arrays
+    try:
+        main_image = face_recognition.load_image_file(main_path + "/" + image_path + "/" + image_name_one)
+        compare_image = face_recognition.load_image_file(main_path + "/" + image_path + "/" + image_name_two)
+    except FileNotFoundError:
+        print("\nPasta nao existe ou possui algum problema de caracter.")
+        try_new_test()
+        continue
 
+    # Get the face encodings for each face in each image file
+    # Since there could be more than one face in each image, it returns a list of encodings.
+    # But since I know each image only has one face, I only care about the first encoding in each image, so I grab index 0.
+    try:
+        main_face_encoding = face_recognition.face_encodings(main_image)[0]
+        compare_face_encoding = face_recognition.face_encodings(compare_image)[0]
+    except IndexError:
+        print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
+        quit()
 
-# carregando imagem com um rosto desconhecido
-unknown_image = face_recognition.load_image_file("two_faces.jpg")
+    known_faces = [
+        main_face_encoding
+    ]
 
-face_locations = face_recognition.face_locations(unknown_image)
-face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+    # results is an array of True/False telling if the unknown face matched anyone in the known_faces array
+    results = face_recognition.compare_faces(known_faces, compare_face_encoding)
 
-
-# convertemos para um formato PIL e entao podemos desenhar com Pillow Library
-pil_image = Image.fromarray(unknown_image)
-draw = ImageDraw.Draw(pil_image)
-
-for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-
-    name = "Unknow"
-
-    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-    best_match_index = np.argmin(face_distances)
-    if matches[best_match_index]:
-        name = known_face_names[best_match_index]
-
-    draw.rectangle(((left, top), (right, bottom)), outline=(0, 0, 255))
-
-    text_width, text_height = draw.textsize(name)
-    draw.rectangle(((left, bottom - text_height - 10), (right, bottom)), fill=(0, 0, 255), outline=(0, 0, 255))
-    draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
-
-del draw
-
-pil_image.save('result.jpg')
+    if results:
+        if results[0] == True:
+            print(f"\nPasta: {image_path}\nResultado: Confere [✓]\nDiretorio: {'/'+ main_path + '/' +image_path }\n---------------------------------------------------")
+            
+        else:
+            print(f"\nPasta: {image_path}\nResultado: Nao confere [x]\nDiretorio: {'/'+ main_path + '/' +image_path }\n---------------------------------------------------")
+    else:
+        print("Não foi possível realizar a comparação de imagens")
+    
+    try_new_test()
