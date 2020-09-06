d8782fdae2a95910bde1be11835bf66feffb436a
Jean Santos <jsantos@igspo.ogmaster.local>
2020-09-06 16:58:09 -0300

    add swagger-ui

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 import os
 from flask import Flask
-from flask-swagger-ui import get_swaggerui_blueprint
+from flask_swagger_ui import get_swaggerui_blueprint
 
 from views.views import my_bp
 
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Seans-Python-Flask-REST-Boilerplate"
+    }
+)
+
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = int(os.getenv('MAX_CONTENT_LENGTH'))
+
 app.register_blueprint(my_bp)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
